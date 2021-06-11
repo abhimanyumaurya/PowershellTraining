4a0dddcbb4c439db29c1aab2c145e6c948907bea
abhimanyumaurya <abhimanyusingh.er@gmail.com>
2021-06-11 20:35:44 +0530

    Create AllowPermissionOnSharedLocation.ps1

---
+++ AllowPermissionOnSharedLocation.ps1
@@ -0,0 +1,18 @@
+﻿$path = "C:\temp"
+
+$ItemPath = Get-ChildItem -path "$Path" -directory -Name -Recurse
+
+foreach($item in $itempath)
+{
+    
+
+   $path1 = "$path\$Item"
+   
+    $acl = Get-Acl $path1
+     $AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule("vf-root\amaurya","Modify","ObjectInherit","None","Allow")
+     $acl.SetAccessRule($AccessRule)
+     $acl | Set-Acl $path1
+
+}
+
+
